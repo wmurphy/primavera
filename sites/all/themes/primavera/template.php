ad3a2cb9288e4976cd79fdffe770cd3b24252f8d
nataliasanchezherrero <natalia.sanchezherrero@gmail.com>
2014-04-15 15:56:39 +0200

    imgs

--- sites/all/themes/primavera/template.php
+++ sites/all/themes/primavera/template.php
@@ -4,3 +4,13 @@
  * @file
  * template.php
  */
+function primavera_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+var_dump($variables);
+  if ($element['#below']) {
+    $sub_menu = drupal_render($element['#below']);
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
